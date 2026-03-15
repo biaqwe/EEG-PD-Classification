8c377163813da3018730cbb30f97fdd0745ed610
Bianca Balas <biancabalas2907@yahoo.com>
2026-03-15 12:47:45 +0200

    docs: add dataset_source description

--- src/state.py
+++ src/state.py
@@ -31,7 +31,7 @@ def init_session_state(): # default session state values
         "page": "Dashboard", # default page
         "dataset_df": None, # loaded .csv dataset
         "dataset_name": None, # name of currently loaded dataset
-        "dataset_source": None,
+        "dataset_source": None, # whether dataset was loaded from csv or mat
         "preproc": PreprocConfig(), # default preprocessing config
         "run_status": "Idle", # app status
         "logs": [], # list of execution logs
